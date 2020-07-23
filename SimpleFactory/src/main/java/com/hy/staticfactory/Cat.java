c1ef06047dcdb586802362c2a400515a6e0daa9a
stephen <ys.stephen@outlook.com>
2020-07-23 23:46:55 +0800

    static factory

---
+++ SimpleFactory/src/main/java/com/hy/staticfactory/Cat.java
@@ -0,0 +1,18 @@
+package com.hy.staticfactory;
+
+/**
+ * author: hubery yang
+ * date: 7/23/2020
+ * description:
+ * title: Cat
+ * version: 1.0
+ *
+ * @Copyright: 2019 Inc. All rights reserved.
+ */
+public class Cat implements Animal {
+
+    @Override
+    public void voice(){
+        System.out.println("MiaoMiao...");
+    }
+}
