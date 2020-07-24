494c4a4000cd67f44e03bce8d611641cd669f7f4
stephen <ys.stephen@outlook.com>
2020-07-24 17:09:02 +0800

    update

---
+++ SimpleFactory/src/main/java/com/hy/staticfactory/Animal.java
@@ -0,0 +1,14 @@
+package com.hy.staticfactory;
+
+/**
+ * author: hubery yang
+ * date: 7/23/2020
+ * description:
+ * title: Animal
+ * version: 1.0
+ *
+ * @Copyright: 2019 Inc. All rights reserved.
+ */
+public interface Animal {
+    public void voice();
+}
